eaca95eb34d63af50e160c31396aac15acfe5a4a
boffman <bkhoffman@gmail.com>
2019-05-08 17:11:41 -0500

    show table function, order products function using inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,11 +6,35 @@ const db = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Rounders22",
+  password: "Spartan117",
   database: "bamazonDB"
 });
 
-db.connect(function(err){
+db.connect(function (err) {
   if (err) throw err;
-  console.log("Connected as id " + db.threadID)
+  console.log("connected as id " + db.threadId);
+  showProducts();
 });
+
+function showProducts(){
+  db.query('SELECT item_id, product_name, price FROM products', (err, res) => {
+    if (err) throw err;
+    printTable(res);
+    console.log("--".repeat(40));
+    orderProducts();
+  });
+  db.end();
+};
+
+function orderProducts(){
+  console.log("What would you like to buy?");
+  inquirer.prompt([{
+    name: "itemID",
+    message: "Pick the id of the item you would like to purchase: "
+  },{
+    name: "quantity",
+    message: "How many would you like to purchase? "
+  }]).then(function(userChoice){
+    
+  })
+};
