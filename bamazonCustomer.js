5e301fb4dccdad6a8fafc545f174bc374310f53e
boffman <bkhoffman@gmail.com>
2019-05-08 15:29:20 -0500

    setup db connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,16 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const {printTable} = require("console-table-printer");
+//setup connection to database
+const db = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Rounders22",
+  database: "bamazonDB"
+});
 
+db.connect(function(err){
+  if (err) throw err;
+  console.log("Connected as id " + db.threadID)
+});
